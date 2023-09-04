d13922aa7ca4fbe4f70c8e9aceb2f8de170fa495
altantolga <altan.tolga@gmail.com>
2023-09-04 22:33:55 +0300

    File Upload Page mhgckhcvgjv

---
+++ src/test/java/com/hectorware/pages/FASRGARGARSGagaergasrg.java
@@ -0,0 +1,12 @@
+package com.hectorware.pages;
+
+import com.hectorware.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class FASRGARGARSGagaergasrg {
+    public FASRGARGARSGagaergasrg() {
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+}
