68afb248bf6377d73a27c20f5b2a1dfa4ddd400f
Emre Taha Özşölen <Gstore255@gmail.com>
2023-09-18 00:37:01 +0300

    Updated
    Notes.feature
    NotesFunction.java
    NotesStepDef.java

--- src/test/java/com/hectorware/pages/NotesFunction.java
+++ src/test/java/com/hectorware/pages/NotesFunction.java
@@ -1,6 +1,41 @@
 package com.hectorware.pages;
 
+import com.hectorware.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class NotesFunction {
+
+    public NotesFunction() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//input[@id='user']")
+    public WebElement inputNameBox;
+
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement inputPasswordBox;
+
+    @FindBy(xpath ="//input[@id='submit-form']" )
+    public WebElement loginButton;
+
+    @FindBy(css = "#appmenu > li:nth-child(9) > a")
+    public WebElement notesButton;
+
+    @FindBy(css = "#notes_new_note")
+    public WebElement createNoteButton;
+
+    @FindBy(xpath = "//div[@class='CodeMirror-code']")
+    public WebElement writingNoteSpace;
+
+    @FindBy(xpath = "//*[@id=\"app-navigation-vue\"]/ul/li[3]/div/div/div/div/button")
+    public WebElement noteFunctionButton;
+
+    @FindBy(xpath = "/html/body/div[3]/div/ul/li/div/div/div/div/button/span")
+    public WebElement deleteNoteButton;
+
+
 }
 
 
